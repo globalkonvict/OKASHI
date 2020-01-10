8ee9d596e08f330993c3e079f24e68e8d4f9a2f4
Sarthak Dwivedi <sarthakwarlock@gmail.com>
2020-01-10 07:09:12 +0530

    Fixed Issue with about screen on low dpi devices, causing users unable to scroll down

--- app/tabs/AboutTab.js
+++ app/tabs/AboutTab.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {web} from 'react-native-communications';
+import {ScrollView} from 'react-native'
 import {
   Container,
   Card,
@@ -10,48 +11,54 @@ import {
   H3,
   Icon,
   Button,
-  Body
 } from 'native-base';
 
 export const AboutTab = () => {
   return (
     <Container>
-      <View style={{alignItems: 'center', marginTop: 20}}>
-        <Thumbnail
-          source={{uri: 'https://i.postimg.cc/ZYj8YrmV/OKASHI.png'}}
-          style={{width: 220, height: 220}}
-        />       
-      </View>
-      <Card style={{marginTop: 20, padding: 10}}>
-        <CardItem>
-          <Text>
-            OKASHI (In Japanese, the word “okashii” (おかしい) Means funny,
-            amusing, comical, laughable, ridiculous,).
-            {`\n\n`}
-            Just an simple app to read jokes available online and not take
-            anything serious.
-            {`\n\n`}
-            P.S. Need internet to work, It's not a Joke!
-          </Text>
-        </CardItem>
-        <CardItem>
-          <H3>Made by Sarthak With </H3>
-          <Icon type="FontAwesome5" name="react" />
-          <Icon type="FontAwesome5" name="node-js" />
-          <Icon type="FontAwesome5" name="brain" />
-        </CardItem>
-        <CardItem style={{flexWrap: 'wrap', marginBottom: 20, justifyContent: 'center'}}>
-        <H3>Code is openSource, feel free to fiddle around!{`\n`}</H3>
-          <Button
-            iconLeft
-            transparent
-            large
-            onPress={e => web('https://github.com/SarthakDwivedi/OKASHI')}>
-            <Icon type="FontAwesome" name="git" />
-            <Text>GITHUB REPO</Text>
-          </Button>          
-        </CardItem>
-      </Card>
+      <ScrollView>
+        <View style={{alignItems: 'center', marginTop: 20}}>
+          <Thumbnail
+            source={{uri: 'https://i.postimg.cc/ZYj8YrmV/OKASHI.png'}}
+            style={{width: 220, height: 220}}
+          />
+        </View>
+        <Card style={{marginTop: 20, padding: 10}}>
+          <CardItem>
+            <Text>
+              OKASHI (In Japanese, the word “okashii” (おかしい) Means funny,
+              amusing, comical, laughable, ridiculous,).
+              {`\n\n`}
+              Just an simple app to read jokes available online and not take
+              anything serious.
+              {`\n\n`}
+              P.S. Need internet to work, It's not a Joke!
+            </Text>
+          </CardItem>
+          <CardItem>
+            <H3>Made by Sarthak With </H3>
+            <Icon type="FontAwesome5" name="react" />
+            <Icon type="FontAwesome5" name="node-js" />
+            <Icon type="FontAwesome5" name="brain" />
+          </CardItem>
+          <CardItem
+            style={{
+              flexWrap: 'wrap',
+              marginBottom: 20,
+              justifyContent: 'center',
+            }}>
+            <H3>Code is openSource, feel free to fiddle around!{`\n`}</H3>
+            <Button
+              iconLeft
+              transparent
+              large
+              onPress={e => web('https://github.com/SarthakDwivedi/OKASHI')}>
+              <Icon type="FontAwesome" name="git" />
+              <Text>GITHUB REPO</Text>
+            </Button>
+          </CardItem>
+        </Card>
+      </ScrollView>
     </Container>
   );
 };
